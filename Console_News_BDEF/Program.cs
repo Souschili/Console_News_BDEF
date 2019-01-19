18be58ca2ede006ca02cf73a0fb76eff5219ba35
Sous Chili <greatdragone75@gmail.com>
2019-01-20 02:46:22 +0400

    Add project files.

---
+++ Console_News_BDEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_News_BDEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
